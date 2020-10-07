77349fd012b415fb3faf5850978a5f4d97e6bbb4
SantiagoFor <santhyzfova@gmail.com>
2020-10-07 14:52:05 -0500

    add bootstrap
    improved search method

--- Datos.js
+++ Datos.js
@@ -1,5 +1,4 @@
-var dato1 = { 'celular': '1234567', 'direccion': 'Av. Falsa 1234'};
-        var dato2 = { 'celular': '7654321', 'direccion': 'Cl. Real 4321'};
-        localStorage.setItem("Fulano Perez", JSON.stringify(dato1));
-        localStorage.setItem("Tomas Edison", JSON.stringify(dato2));
-        actualizarDatos();
+var dato1 = { 'celular': '1234567', 'direccion': 'AV. FALSA 1234'};
+        var dato2 = { 'celular': '7654321', 'direccion': 'CL. REAL 4321'};
+        localStorage.setItem("FULANOPEREZ", JSON.stringify(dato1));
+        localStorage.setItem("TOMAS EDISON", JSON.stringify(dato2));
--- localStorage.js
+++ localStorage.js
@@ -1,29 +1,55 @@
+    window.onload= cargaDatos();
     function nuevo(){
         let formulario=`
-        <table>
-            <tr>
-                <th>
+        <div class="card border-dark mb-12" style="margin: 10px 10px 10px 10px;">
+            <div class="card-body">           
+                <form>
+                    <div class="form-row">
+                        <div class=" col-3">
+                        <label>NOMBRE:</label>
+                        <label>CELULAR:</label> 
+                        <label> DIRECCION: </label>
+                        </div>
+                        <div class=" col-6">
+                            <input type="text" required class="form-control form-control-sm" placeholder="NOMBRE" id="nombre">                       
+                            <input type="number"class="form-control form-control-sm" placeholder="CELULAR" id="celular">
+                            <input type="text" class="form-control form-control-sm" placeholder="DIRECCION" id="direccion">   
+                        </div>  
+                    </div>
+                </form>
+                            <button type="button" class="offset-3  btn btn-sm btn-info"onclick="validate()">Guardar</button>
+                            <button type="button" class="btn btn-sm btn-danger"onclick="cancelar()">Cancelar</button>
                 
-                    Nombre:
-                    <input type="text" class="form-control" placeholder="Nombre" id="nombre">
-                    <br>
-                    Numero:
-                    <input type="text" class="form-control" placeholder="Movil" id="celular">
-                    <br>
-                    Direccion:
-                    <input type="text" class="form-control" placeholder="Direccion" id="direccion" >
-                    <br>
-                <button type="button" onclick="guardarDato()">Guardar</button>
-                    </th>
-                </tr>
-            </table>`;
+            </div>
+            
+        </div>`;
 
     document.getElementById('agregar').innerHTML = formulario;
     }
+    function validate(){
+        var nombre=document.getElementById("nombre").value;
+        var celular=document.getElementById("celular").value;
+        var direccion=document.getElementById("direccion").value;
+        if(nombre==""){
+            alert("Debe ingresar nombre");
+        }
+        if(celular==""){
+            alert("Debe ingresar celular ");
+        }else if(celular.length<6 || celular.length>10){
+            alert("El celular debe tener entre 6 y 10 numeros");
+        }
+        if(direccion==""){
+            alert("Debe ingresar la Direccion");
+        }
+        guardarDato();
+    }
+    function cancelar(){
+        document.getElementById('agregar').innerHTML = "";
+    }
     function guardarDato() {
-        const nombre = document.getElementById("nombre").value;
-        const celular = document.getElementById("celular").value;
-        const direccion = document.getElementById("direccion").value;
+        const nombre = document.getElementById("nombre").value.toUpperCase();
+        const celular = document.getElementById("celular").value.toUpperCase();
+        const direccion =document.getElementById("direccion").value.toUpperCase();
         const datos = {
             'celular': celular,
             'direccion': direccion
@@ -35,14 +61,14 @@
         document.getElementById("direccion").value = "";
         formulario="";
         document.getElementById('agregar').innerHTML = formulario;
-        actualizarDatos();
+       cargaDatos();
     }
     function eliminarDato(key) {
         localStorage.removeItem(key);
-        actualizarDatos();
+       cargaDatos();
     }
     function editar(dato){
-       const nombre = dato;
+        const nombre = dato;
         const celular = document.getElementById("celular".concat(dato)).value;
         const direccion = document.getElementById("direccion".concat(dato)).value;
         const datos = {
@@ -50,72 +76,95 @@
             'direccion': direccion
         };
         localStorage.setItem(nombre, JSON.stringify(datos));
-        actualizarDatos();
+        cargaDatos();
     }
-    function actualizarDatos() {
+    function cargaDatos(){
         let elementos = "";
-        const campobusqueda = document.getElementById("busqueda").value;
         if (localStorage.length === 0) {
             elementos += 'Sin Contactos';
         } else {
-            if(campobusqueda===""){
+            for (let i = 0; i <= localStorage.length - 1; i++) {
+                const key = localStorage.key(i);
+                let datos = localStorage.getItem(key);
+                datos = JSON.parse(datos);
+                elementos += `
+                <div class="card border-dark mb-12" style="margin: 10px 10px 10px 10px;">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-row">
+                                <div class=" col-3">
+                                    <label>NOMBRE:</label>
+                                    <label>CELULAR:</label>
+                                    <label>DIRECCION:</label>
+                                </div>
+                                <div class=" col-6">
+                                    <input type="text" class="form-control form-control-sm" value="${key}" id="${key}" name="${key}">
+                                    <input type="text" class="form-control form-control-sm" value="${datos.celular}" id="celular${key}">                                         
+                                    <input type="text" class="form-control form-control-sm" value="${datos.direccion}" id="direccion${key}">
+                                </div>
+                                <div class="col-md-2">
+                                    <button type="button" class=" btn btn-sm btn-danger" onclick="eliminarDato('${key}')"> Eliminar</button>    <br>
+                                    <button type="button" class="btn btn-sm btn-warning" onclick="editar('${key}')"> Editar</button> </th>
+                                </div>
+                            </div> 
+                        </form>       
+                    </div>   
+                </div>
+                `;
+            }
+            document.getElementById('contactos').innerHTML = elementos;
+        }
+    }
+    function Buscar() {
+        let elementos = "";
+        let mensage = "";
+        campobusqueda = document.getElementById("busqueda").value.toUpperCase();
+        if (localStorage.length === 0) {
+            elementos += 'Sin Contactos';
+        } else {
+            var resultados=0;
                 for (let i = 0; i <= localStorage.length - 1; i++) {
                     const key = localStorage.key(i);
                     let datos = localStorage.getItem(key);
                     datos = JSON.parse(datos);
-                    elementos += `
-                    <table>
-                            <tr>
-                                <th>
-                                    Nombre:<input type="text" value="${key}" id="${key}" name="${key}"><br>
-                                    Direccion:<input type="text" value="${datos.direccion}" id="direccion${key}">
-                                    <br>
-                                    Celular:<input type="text" value="${datos.celular}" id="celular${key}">
-                                    <br>
-                                   
-                                </th>
-                                <th>
-                                    <button type="button" onclick="eliminarDato('${key}')"> Eliminar</button><br>
-                                    <button type="button" onclick="editar('${key}')"> Editar</button> 
-                                </th>
-                            </tr>
-                        </table>`;
-                }
-            }else{
-                for (let i = 0; i <= localStorage.length - 1; i++) {
-                    const key = localStorage.key(i);
-                    let datos = localStorage.getItem(key);
-                    datos = JSON.parse(datos);
-                    var resultados=0;
-    
-                    if(campobusqueda==key){
-                        console.log("help");
+                    
+                    if(0<=key.indexOf(campobusqueda)){
+                        resultados=1;
+                        console.log("Encontrado");
                         elementos += `
-                        <table>
-                        <tr>
-                            <th>
-                                Nombre:<input type="text" value="${key}" id="${key}" name="${key}"><br>
-                                Direccion:<input type="text" value="${datos.direccion}" id="direccion${key}">
-                                <br>
-                                Celular:<input type="text" value="${datos.celular}" id="celular${key}">
-                                <br>
-                                
-                            </th>
-                            <th>
-                                <button type="button" onclick="eliminarDato('${key}')"> Eliminar</button><br>
-                                <button type="button" onclick="editar('${key}')"> Editar</button> 
-                            </th>
-                        </tr>
-                    </table>`;
-                    }else if( i == localStorage.length - 1 &&  campobusqueda!=key){
-                        elementos += `<p>El nombre debe estar escrito igual que en el contacto</p>`;
+                        <div class="card border-dark mb-12" style="margin: 10px 10px 10px 10px;">
+                            <div class="card-body">
+                                <form>
+                                    <div class="form-row">
+                                        <div class=" col-3">
+                                            <label>NOMBRE:</label>
+                                            <label>CELULAR:</label>
+                                            <label>DIRECCION:</label>
+                                        </div>
+                                        <div class=" col-6">
+                                            <input type="text" class="form-control form-control-sm" value="${key}" id="${key}" name="${key}">
+                                            <input type="text" class="form-control form-control-sm" value="${datos.celular}" id="celular${key}">                                         
+                                            <input type="text" class="form-control form-control-sm" value="${datos.direccion}" id="direccion${key}">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <button type="button" class=" btn btn-sm btn-danger" onclick="eliminarDato('${key}')"> Eliminar</button>    <br>
+                                            <button type="button" class="btn btn-sm btn-warning" onclick="editar('${key}')"> Editar</button> </th>
+                                        </div>
+                                    </div> 
+                                </form>       
+                            </div>   
+                        </div>
+                    `;
+                    document.getElementById('contactos').innerHTML = elementos;
                     }
                 }
+                if(resultados===0){
+                    alert("No encontramos lo que buscabas");
+                    cargaDatos();
+                 }
                 document.getElementById("busqueda").value = "";
-
-            }
         }
-        document.getElementById('contactos').innerHTML = elementos;
+            
     }
 
 
